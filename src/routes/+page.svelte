15c0c7ee6c8a8c9c52a11776454c1b3e27f24b50
Richard <richard@stromer.org>
2024-10-18 14:01:22 +0200

    add basic styling for container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="container mx-auto py-12">
+	<h1 class="pb-2 text-xl">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
