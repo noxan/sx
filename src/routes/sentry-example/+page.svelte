e36847a4b97324057ffab3c1c23053dde97c12cf
Richard <richard@stromer.org>
2024-10-18 19:04:37 +0200

    remove sentry import from demo page

--- src/routes/sentry-example/+page.svelte
+++ src/routes/sentry-example/+page.svelte
@@ -4,21 +4,11 @@ Feel free to delete this file and the entire sentry route.
 -->
 
 <script>
-	import * as Sentry from '@sentry/sveltekit';
-
-	function getSentryData() {
-		Sentry.startSpan(
-			{
-				name: 'Example Frontend Span',
-				op: 'test'
-			},
-			async () => {
-				const res = await fetch('/sentry-example');
-				if (!res.ok) {
-					throw new Error('Sentry Example Frontend Error');
-				}
-			}
-		);
+	async function getSentryData() {
+		const res = await fetch('/sentry-example');
+		if (!res.ok) {
+			throw new Error('Sentry Example Frontend Error');
+		}
 	}
 </script>
 
