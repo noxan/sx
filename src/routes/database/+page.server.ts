4a7df3a3bcb8ed4d36849b3076536e17b2e1dad0
Richard <richard@stromer.org>
2024-10-18 16:29:36 +0200

    load users via drizzle

--- src/routes/database/+page.server.ts
+++ src/routes/database/+page.server.ts
@@ -1,7 +1,12 @@
+import { db } from '$lib/server/db';
+import { user } from '$lib/server/db/schema';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
+	const users = await db.select().from(user);
+
 	return {
+		users,
 		test: 'bla'
 	};
 };
