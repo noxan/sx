a4fbbb5fab5c2e6bde382fd16ae43bea3a884e4d
Richard <richard@stromer.org>
2024-10-18 14:11:09 +0200

    add section around demo content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <main class="container mx-auto py-12">
-	<h1 class="pb-2 text-xl">Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<section class="my-6">
+		<h1 class="pb-2 text-xl">Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</section>
 </main>
