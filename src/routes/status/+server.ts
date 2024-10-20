13273b00ea3a435ce458d53ecfe4b5028923cbcd
Richard <richard@stromer.org>
2024-10-20 16:44:31 +0200

    expose cf pages and commit sha vars

--- src/routes/status/+server.ts
+++ src/routes/status/+server.ts
@@ -2,7 +2,11 @@ import { NODE_ENV } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
+const {CF_PAGES, CF_PAGES_COMMIT_SHA} = process.env;
+
 export const GET: RequestHandler = () =>
 	json({
+		CF_PAGES,
+		CF_PAGES_COMMIT_SHA,
 		environment: NODE_ENV,
 	});
