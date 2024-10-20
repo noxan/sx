06ead2b5a5ae394a9b8b62afac1184405a6fea29
Richard <richard@stromer.org>
2024-10-20 15:46:43 +0200

    extract database configs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -11,20 +11,22 @@ const config = {
 	strict: true
 } satisfies Config;
 
+const localConfig = {
+	dbCredentials: {
+		url: LOCAL_DATABASE_URL
+	}
+};
+
+const remoteConfig = {
+	driver: 'd1-http',
+	dbCredentials: {
+		// accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		// databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+		// token: process.env.CLOUDFLARE_D1_TOKEN!
+	}
+};
+
 export default defineConfig({
 	...config,
-	...(LOCAL_DATABASE_URL
-		? {
-				dbCredentials: {
-					url: LOCAL_DATABASE_URL
-				}
-			}
-		: {
-				driver: 'd1-http',
-				dbCredentials: {
-					// accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-					// databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-					// token: process.env.CLOUDFLARE_D1_TOKEN!
-				}
-			})
+	...(LOCAL_DATABASE_URL ? localConfig : remoteConfig)
 });
