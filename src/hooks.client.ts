8fcca30e753eec99f9d9869c0a5bd1d76d42bd2d
Richard <richard@stromer.org>
2024-10-20 17:18:24 +0200

    use import.meta.env.PROD for sentry config

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,8 @@
-import { NODE_ENV } from '$env/static/private';
 import { init } from '@jill64/sentry-sveltekit-cloudflare/client';
 
 const handleErrorWithSentry = init(
 	'https://2673cb3aed1f9088a1516478698e54fb@o4508105785868288.ingest.de.sentry.io/4508144247177296',
-	{ sentryOptions: { enabled: NODE_ENV !== 'development' } },
+	{ sentryOptions: { enabled: import.meta.env.PROD } },
 );
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,9 @@
-import { NODE_ENV } from '$env/static/private';
 import { init } from '@jill64/sentry-sveltekit-cloudflare/server';
 import { handle as authHandle } from './auth';
 
 const { onHandle, onError } = init(
 	'https://2673cb3aed1f9088a1516478698e54fb@o4508105785868288.ingest.de.sentry.io/4508144247177296',
-	{ toucanOptions: { enabled: NODE_ENV !== 'development' } },
+	{ toucanOptions: { enabled: import.meta.env.PROD } },
 );
 
 export const handleError = onError();
