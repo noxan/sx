b09af28a183acea9db3e95bcc299ddb1620cc0fe
Richard <richard@stromer.org>
2024-10-20 15:27:52 +0200

    configure drizzlekit to work with local d1

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,30 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+import type { Config } from 'drizzle-kit';
 
-export default defineConfig({
+const { LOCAL_DATABASE_URL } = process.env;
+
+const config = {
 	schema: './src/lib/server/db/schema.ts',
-
-	dbCredentials: {
-		url: process.env.DATABASE_URL
-	},
+	dialect: 'sqlite',
 
 	verbose: true,
-	strict: true,
-	dialect: 'sqlite'
+	strict: true
+} satisfies Config;
+
+export default defineConfig({
+	...config,
+	...(LOCAL_DATABASE_URL
+		? {
+				dbCredentials: {
+					url: LOCAL_DATABASE_URL
+				}
+			}
+		: {
+				driver: 'd1-http',
+				dbCredentials: {
+					// accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+					// databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+					// token: process.env.CLOUDFLARE_D1_TOKEN!
+				}
+			})
 });
