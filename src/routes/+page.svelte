2876b4fa5ddcdcb04cbf8e365f37e02ced8aea8b
Richard <richard@stromer.org>
2024-12-24 17:30:29 +0100

    fix page app/stores deprecation warning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { signIn, signOut } from '@auth/sveltekit/client';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 </script>
 
 <section class="my-6">
@@ -21,16 +21,16 @@
 
 <section class="my-6">
 	<h2 class="pb-2 text-lg">Authentication</h2>
-	{#if $page.data.session}
+	{#if page.data.session}
 		<div class="flex flex-row items-center gap-4">
-			{#if $page.data.session.user?.image}
-				<img src={$page.data.session.user.image} alt="User avatar" class="h-24 w-24 rounded-full" />
+			{#if page.data.session.user?.image}
+				<img src={page.data.session.user.image} alt="User avatar" class="h-24 w-24 rounded-full" />
 			{:else}
-				<div class="h-24 w-24 rounded-full bg-gray-200" />
+				<div class="h-24 w-24 rounded-full bg-gray-200"></div>
 			{/if}
 			<div>
-				<p>{$page.data.session.user?.name}</p>
-				<p class="text-sm text-gray-600">{$page.data.session.user?.email}</p>
+				<p>{page.data.session.user?.name}</p>
+				<p class="text-sm text-gray-600">{page.data.session.user?.email}</p>
 				<button on:click={() => signOut()} class="mt-1">Sign out</button>
 			</div>
 		</div>
